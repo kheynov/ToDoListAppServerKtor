e772e267a81a11801e8faecf18b86efce00c50b7
Vladislav Kheynov <lcastory86@gmail.com>
2021-12-13 23:17:11 +0700

    feat: add in memory repository calls

--- src/main/kotlin/ru/kheynov/plugins/Routing.kt
+++ src/main/kotlin/ru/kheynov/plugins/Routing.kt
@@ -1,16 +1,45 @@
 package ru.kheynov.plugins
 
-import io.ktor.routing.*
-import io.ktor.http.*
 import io.ktor.application.*
+import io.ktor.http.*
 import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.routing.*
+import ru.kheynov.entities.Todo
+import ru.kheynov.repository.InMemoryTodoRepositoryImpl
+import ru.kheynov.repository.TodoRepository
 
 fun Application.configureRouting() {
 
     routing {
+        val repository: TodoRepository = InMemoryTodoRepositoryImpl()
         get("/") {
-            call.respondText("Hello World!")
+            call.respondText("Todo Application")
+        }
+        get("/todos") {
+            call.respond(repository.getAllTodos())
+        }
+        get("/todos/{id}") {
+            val id = call.parameters["id"]?.toIntOrNull()
+            if (id == null) {
+                call.respond(HttpStatusCode.BadRequest, "Id not specified correctly")
+                return@get
+            }
+            val todo = repository.getTodo(id)
+            if (todo == null) {
+                call.respond(HttpStatusCode.NotFound, "Todo not found")
+            } else {
+                call.respond(HttpStatusCode.OK, todo)
+            }
+
+        }
+        post("/todos") {
+
+        }
+        put("/todos/{id}") {
+
+        }
+        delete("/todos/{id}") {
+
         }
     }
 }
